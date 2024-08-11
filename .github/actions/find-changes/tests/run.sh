9d948f11a86380750db1c2102ef1c334497a7018
anttiharju <git@anttiharju.dev>
2024-08-11 10:39:36 +0300

    Setup more test infra

--- .github/actions/find-changes/tests/run.sh
+++ .github/actions/find-changes/tests/run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 BASE_DIR=$(cd "$(dirname "$0")" && pwd)
+trap 'rm -rf "$BASE_DIR/tmp"' EXIT
 
 run_script() {
 	local SCRIPT_PATH="$1"
 	if [ -x "$SCRIPT_PATH" ]; then
+		rm -rf "$BASE_DIR/tmp"
+		mkdir -p "$BASE_DIR/tmp"
+		(cd $BASE_DIR/tmp && git init && touch README.md && git add README.md && git commit -m "Initial commit")
 		"$SCRIPT_PATH"
 	else
 		echo "Error: $SCRIPT_PATH is not executable or does not exist"
