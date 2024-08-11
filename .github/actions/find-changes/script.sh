75416fcb0c9ab0136bf53e83c6b9575a0f36015c
anttiharju <git@anttiharju.dev>
2024-08-11 18:15:13 +0300

    Fix script.sh

--- .github/actions/find-changes/script.sh
+++ .github/actions/find-changes/script.sh
@@ -4,16 +4,26 @@ default_branch="$1"
 
 # Check if current branch is the default branch
 current_branch=$(git rev-parse --abbrev-ref HEAD)
+base_ref=""
 if [ "$current_branch" = "$default_branch" ]; then
-	# Increase checkout depth
-	git fetch origin "$default_branch:$default_branch"
+	if git log -1 --pretty=%P | grep -q ' '; then
+		git fetch --depth=2 origin "$default_branch:$default_branch"
+		merge_ref=$(git log --merges -n 1 --pretty=format:"%H")
+		base_ref=$(git rev-parse HEAD^) # merge commit
+	else
+		latest_commit=$(git rev-parse HEAD)
+		base_ref=$(git merge-base HEAD "$default_branch")
+		if [ "$latest_commit" = "$base_ref" ]; then
+			base_ref=$(git rev-parse HEAD^) # squash
+		else
+			base_ref=$(git merge-base HEAD "$default_branch") # rebase
+		fi
+	fi
 else
 	# Fetch latest state of default branch branch
 	git fetch --depth=1 origin "$default_branch:$default_branch"
+	base_ref=$(git merge-base HEAD "$default_branch")
 fi
-
-# Determine the base ref
-base_ref=$(git merge-base HEAD "origin/$default_branch")
 
 # Get the list of changed first-level directories compared to the base ref
 changes=$(git diff --name-only "$base_ref...HEAD" | awk -F/ '{print $1}' | sort --unique)
