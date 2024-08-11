baed4b45cfc8069ae80315069ee4cbc7073f1aee
anttiharju <antti@harju.io>
2024-08-11 16:55:47 +0300

    Implement nonlinear history test
    These aren't 1:1 with GitHub implementation but to my understanding
    close enough.

--- .github/actions/find-changes/test/pushes/have nonlinear history.sh
+++ .github/actions/find-changes/test/pushes/have nonlinear history.sh
@@ -1,3 +1,31 @@
 #!/bin/sh
 
-echo "I am merge_commit.sh!"
+# * common ancestor
+# |
+# |‾‾‾‾\
+# |\    \
+# | * A  * B
+# |/    /
+# * A  /
+# |___/
+# |
+# * B <- only B should be found, even though A is between common ancestor and B
+
+git branch A
+git branch B
+
+git switch A
+mkdir A
+touch A/a
+git add A/a
+git commit -m "Add a"
+git switch -
+git merge A
+
+git switch B
+mkdir B
+touch B/b
+git add B/b
+git commit -m "Add b"
+git switch -
+git merge B
