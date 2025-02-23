384d129d221620e143e56fecc0831fc8f9e6aae2
Antti Harju <antti@harju.io>
2025-02-23 11:54:58 +0200

    Simplify tree and usage

--- run-shellcheck/test.py
+++ run-shellcheck/test.py
@@ -2,6 +2,6 @@
 
 # Actual action command
 # git ls-files -z | xargs -0 file | grep "script text executable" | cut -d: -f1 | xargs shellcheck -x
-# we want to mimick git ls-files with data/got.txt
-# and test the grep command to see if we end up with data/want.txt
+# we want to mimick git ls-files with testdata/got.txt
+# and test the grep command to see if we end up with testdata/want.txt
 # do not actually run shellcheck or cut
