8dc573e2a5c81285b0ef3460e72bfb772ec19e8e
Antti Harju <antti@harju.io>
2025-04-05 23:18:08 +0300

    Embed shell scripts in yaml

--- shellcheck/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-curl -sSL "$URL" | tar -xJ -C "$TMP" --strip-components=1
-cp "$TMP/shellcheck" /usr/local/bin/
--- shellcheck/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-shellcheck --version
-git ls-files -z '*.sh' '*.bash' '*.dash' '*.ksh' | xargs -0 shellcheck --color=always --source-path=SCRIPTDIR
