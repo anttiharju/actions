3d012362320ec08dbff6066852f68358d9b3bd45
Antti Harju <antti@harju.io>
2025-04-06 13:09:32 +0300

    Add test scaffolding

---
+++ find-changes/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+A simple Hello World program to test the Python environment.
+"""
+
+def main():
+    """Print a greeting message to the console."""
+    print("Hello, World!")
+    print("This is a test script for the find-changes action.")
+
+
+if __name__ == "__main__":
+    main()
